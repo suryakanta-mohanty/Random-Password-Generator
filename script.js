5a8c3d4e046c850c2219114b41a48612c6dbebed
suryakanta-mohanty <suryakantamohanty855@gmail.com>
2024-06-12 17:27:55 +0530

    add js file

---
+++ script.js
@@ -0,0 +1,30 @@
+const passwordBox = document.getElementById('password');
+const length = 10;
+
+const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowerCase = upperCase.toLowerCase();
+
+const number = '0123456789';
+const symbol = '!@#$%^&*()_+=?/~[]{}<>|';
+
+const allChars = upperCase + lowerCase + number + symbol;
+
+function createPassword(){
+  let password = '';
+
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+  password += symbol[Math.floor(Math.random() * symbol.length)];
+  
+  while(length > password.length){
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+
+  passwordBox.value = password;
+  
+}
+
+document.querySelector('.js-btn').addEventListener('click', ()=>{
+  createPassword();
+});
